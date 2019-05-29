fc5714a8b935bdbb107acfa4b0f45a779381af6b
siberianLvolk <callmevolk@gmail.com>
2019-05-29 20:17:42 +0200

    test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,6 @@ const IndexPage = () => (
 		<Head title="Home" />
 		<h1>Hello.</h1>
 		<h2>I'm Ljubiša, a React developer living in Prnjavor</h2>
-
-		<p>Need a developer? <Link to='/contact'>Contact me</Link>.</p>	
 	</Layout>
 )
 
